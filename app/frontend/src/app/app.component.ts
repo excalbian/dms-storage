47b6099b175979ff7b664ebdf0c7d345feb4b117
skyspook <skyspook@dallasmakerspace.org>
2021-08-13 11:04:05 -0500

    Update project names and docker development

--- app/frontend/src/app/app.component.ts
+++ app/frontend/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'dms-storage';
 }
